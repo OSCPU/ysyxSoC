1e1ae8811332c9751059e29091dfd819c4667bf9
Chen Lu <181250012@smail.nju.edu.cn>
2021-09-09 20:20:25 +0800

    ysyx/program,loader, fix program size

--- ysyx/program/src/loader/loader.c
+++ ysyx/program/src/loader/loader.c
@@ -7,11 +7,11 @@ extern uint32_t program_end;
 extern uint32_t _pmem_start;
 #define program_SIZE ((&program_end) - (&program_start))
 
-void main(){
+int main(){
     uint32_t* program = (uint32_t*)&program_start;
     uint32_t* pmem    = (uint32_t*)&_pmem_start;
     putstr("Loading program of size ");
-    printf("%d: expect 128 \'#\'\n", (uint32_t)program_SIZE);
+    printf("%d: expect 128 \'#\'\n", (uint32_t)program_SIZE * sizeof(uint32_t));
     putstr("Loading.....");
     uint32_t step = (uint32_t)(&program_end - &program_start) / 128;
     uint32_t* pre = program;
@@ -27,4 +27,5 @@ void main(){
     asm volatile("fence.i");
     int (*f)() = (int (*)())(0x80000000);
     f();
+    return 0;
 }
