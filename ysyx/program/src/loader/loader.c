93bda6d6ff14ffb27bb135fdcfed91ca06cd42f6
Chen Lu <181250012@smail.nju.edu.cn>
2021-09-09 16:52:18 +0800

    ysyx/program: update loader

--- ysyx/program/src/loader/loader.c
+++ ysyx/program/src/loader/loader.c
@@ -10,9 +10,20 @@ extern uint32_t _pmem_start;
 void main(){
     uint32_t* program = (uint32_t*)&program_start;
     uint32_t* pmem    = (uint32_t*)&_pmem_start;
+    putstr("Loading program of size ");
+    printf("%d: expect 128 \'#\'\n", (uint32_t)program_SIZE);
+    putstr("Loading.....");
+    uint32_t step = (uint32_t)(&program_end - &program_start) / 128;
+    uint32_t* pre = program;
+
     while(program < &program_end){
         *pmem++ = *program++;
+        if((uint32_t)(program - pre) >= step){
+            putch('#');
+            pre = program;
+        }
     }
+    putstr("\nLoad Finished\n");
     asm volatile("fence.i");
     int (*f)() = (int (*)())(0x80000000);
     f();
