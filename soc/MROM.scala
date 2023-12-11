abfed1145c2c5a3245d6ecc01164a9b80c31c642
Zihao Yu <yuzihao@ict.ac.cn>
2023-12-11 12:56:09 +0800

    soc,AXI4MROM: put comment into the message of assertion

--- soc/MROM.scala
+++ soc/MROM.scala
@@ -58,7 +58,7 @@ class AXI4MROM(address: Seq[AddressSet])(implicit p: Parameters) extends LazyMod
     mrom.io.raddr := in.ar.bits.addr
     mrom.io.ren := in.ar.fire
     in.ar.ready := true.B
-    assert(!(in.ar.fire && in.ar.bits.size === 3.U)) // do not support 8 byte transfter
+    assert(!(in.ar.fire && in.ar.bits.size === 3.U), "do not support 8 byte transfter")
 
     in.r.bits.data := RegEnable(Fill(2, mrom.io.rdata), in.ar.fire)
     in.r.bits.id := RegEnable(in.ar.bits.id, in.ar.fire)
