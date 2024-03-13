5d0cacdeecb1e0b24c6786e25624c6bb2d85aab9
Zihao Yu <yuzihao@ict.ac.cn>
2024-03-13 08:11:22 +0800

    perip,uart16550: fix the condition of $write()

--- perip/uart16550/rtl/uart_tfifo.v
+++ perip/uart16550/rtl/uart_tfifo.v
@@ -206,12 +206,12 @@ begin
         bottom <= #1 'b0;
         count  <= #1 'b0;
     end else begin
-        if (push) $write("%c", data_in);
         case ({push, pop})
         2'b10 : if (count<fifo_depth)  // overrun condition
             begin
                 top   <= #1 top_plus_1;
                 count <= #1 count + 1'b1;
+                $write("%c", data_in);
             end
         2'b01 : if(count>0)
             begin
@@ -221,6 +221,7 @@ begin
         2'b11 : begin
                 bottom <= #1 bottom + 1'b1;
                 top    <= #1 top_plus_1;
+                $write("%c", data_in);
                 end
         default: ;
         endcase
