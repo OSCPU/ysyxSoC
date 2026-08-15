1fec15d9e4c7f1add6129775f9ad0eb5e9de3620
Zihao Yu <yuzihao@ict.ac.cn>
2026-08-15 17:14:16 +0800

    amba: remove delayer
    * Now we can set the frequency ratio between clock and cpuClock to 1:N
      in verilator. This will be simpler and more precise than delayer.

--- src/SoC.scala
+++ src/SoC.scala
@@ -136,8 +136,7 @@ class ysyxSoC(implicit p: Parameters) extends LazyModule {
     } else AXI4Buffer()
     tmpNode2 := AXI4UserYanker(Some(1)) := AXI4Fragmenter() := xbar
   }
-  val apbDelayer = if (Config.hasHomework) APBDelayer() else APBIdentityNode()
-  apbxbar := apbDelayer := AXI4ToAPB() := tmpNode
+  apbxbar := AXI4ToAPB() := tmpNode
   val cdc = if (Config.hasCDC) Some(LazyModule(new AXI4RationalCrossing(FastToSlow))) else None
   val cdcNode = if (cdc != None) cdc.get.node else AXI4IdentityNode()
   xbar := cdcNode := cpu.masterNode
